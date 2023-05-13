0c1713d23b6e291f166fd4b3fd9a546a0c4a0954
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-05-13 21:37:59 +0300

    Add data validator for the view

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -5,7 +5,12 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
-    # print(request.GET)  # Напечатаем
-    form = BirthdayForm()
+    if request.GET:
+        # print(request.GET)  # Напечатаем
+        form = BirthdayForm(request.GET)
+        if form.is_valid():
+            pass
+    else:
+        form = BirthdayForm()
     context = {'form': form}
     return render(request, 'birthday/birthday.html', context=context)
