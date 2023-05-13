1f74b8e1b69c124909fc91cca2c61755f39ab13a
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-05-13 21:25:23 +0300

    Add data returning and optimize the template

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -5,6 +5,7 @@ from .forms import BirthdayForm
 
 
 def birthday(request):
+    # print(request.GET)  # Напечатаем
     form = BirthdayForm()
     context = {'form': form}
     return render(request, 'birthday/birthday.html', context=context)
