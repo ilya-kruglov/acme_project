590d90be92e9e41e4d4df10f2d333e08bd958b13
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-05-18 17:45:41 +0300

    Add TemplateView for static page

--- acme_project/pages/views.py
+++ acme_project/pages/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+# pages/views.py
+from typing import Any, Dict
+from django.views.generic import TemplateView
+
+from birthday.models import Birthday
 
 
-def homepage(request):
-    return render(request, 'pages/index.html')
+class HomePage(TemplateView):
+    template_name = 'pages/index.html'
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        context['total_count'] = Birthday.objects.count()
+        return context
