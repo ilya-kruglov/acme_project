590a7c2632ed0e1ca2415626cfbbefb2d012c66b
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-05-20 19:29:53 +0300

    Add auth check before allow users create, update or delete objects

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,5 +1,8 @@
 # birthday/views.py
 from typing import Any, Dict
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
 from django.views.generic import (
     CreateView, DeleteView, DetailView, ListView, UpdateView
 )
@@ -8,6 +11,11 @@ from django.urls import reverse_lazy
 from .forms import BirthdayForm
 from .models import Birthday
 from .utils import calculate_birthday_countdown
+
+
+@login_required
+def simple_view(request):
+    return HttpResponse('Страница для залогиненных пользователей!')
 
 
 class BirthdayListView(ListView):
@@ -19,17 +27,17 @@ class BirthdayListView(ListView):
     paginate_by = 3
 
 
-class BirthdayCreateView(CreateView):
+class BirthdayCreateView(LoginRequiredMixin, CreateView):
     model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayUpdateView(UpdateView):
+class BirthdayUpdateView(LoginRequiredMixin, UpdateView):
     model = Birthday
     form_class = BirthdayForm
 
 
-class BirthdayDeleteView(DeleteView):
+class BirthdayDeleteView(LoginRequiredMixin, DeleteView):
     model = Birthday
     success_url = reverse_lazy('birthday:list')
 
