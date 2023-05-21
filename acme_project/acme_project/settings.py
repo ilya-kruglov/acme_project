d38750063a69188a3a9d5b46913a13e2f9ef4f37
Ilya Kruglov <ilya.kruglove@yandex.ru>
2023-05-21 19:28:51 +0300

    Add Django Debug Toolbar

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
     'django_bootstrap5',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -32,6 +33,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = [
+    '127.0.0.1',
 ]
 
 ROOT_URLCONF = 'acme_project.urls'
--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -20,6 +20,12 @@ urlpatterns = [
         name='registration',
     ),
     path('birthday/', include('birthday.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'core.views.page_not_found'
